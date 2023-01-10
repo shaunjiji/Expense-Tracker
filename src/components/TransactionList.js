bbacf17be3220f64cce0ff1cd7279d3329771f52
Shaun Jiji <shaunjiji@Shauns-MacBook-Pro-2.local>
2023-01-10 17:19:27 +0530

    add state in addTransaction component

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -5,9 +5,9 @@ const TransactionList = () => {
     <>
       <h3>History</h3>
       <ul id="list" class="list">
-        <li class="minus">
+        <li className="minus">
           Cash <span>-$400</span>
-          <button class="delete-btn">x</button>
+          <button className="delete-btn">x</button>
         </li>
       </ul>
     </>
