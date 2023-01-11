d8727a34f10dcbefe9fd1a10899cb9ff3d0c78ed
Shaun Jiji <shaunjiji@Shauns-MacBook-Pro-2.local>
2023-01-11 19:28:27 +0530

    create provider component

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -13,3 +13,18 @@ const initialState = {
 
 //Create context
 export const GlobalContext = createContext(initialState);
+
+//Provider component
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
